96395bed40e2c429493b91e95a3858382d73fcf3
Tadashi0000001 <imeshtadashi@gmail.com>
2025-06-10 22:24:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,13 +11,13 @@ module.exports = {
     whatsapp: {
         groupBInviteLink: "https://chat.whatsapp.com/JnenfkxOzH6JefRmeaSo0M",
         successMessage: "*ㄚᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴘᴜꜱʜ ᴀʟᴇʀᴛ* ‼️\n\n*丂ᴜᴄᴄᴇꜱ! ɪ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ɢʀᴏᴜᴘꜱ ᴀɴᴅ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ*\n\nGroup A: {groupAName}\nGroup B: {groupBName}\n\n> *Ｐᴏᴡᴇʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ* ‼️",
-        errorMessage: "*千ᴀɪʟᴇᴅ ᴛᴏ ᴊᴏɪɴ ɢʀᴏᴜᴘꜱ ᴏʀ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ. Ｅʀʀᴏʀ: {ᴇʀʀᴏʀ}* ✖️*",
-        broadcastCaption: `**卂ꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ ʙʀᴏᴀᴅᴄᴀꜱᴛ ꜱʏꜱᴛᴇᴍ එක මගින් ලැබෙන 爪ᴇꜱꜱᴀɢᴇ එකකි ‼️**\n\n*Group Name :* {groupName}\n\n{caption}\n\n*乃ʀᴏᴀᴅᴄᴀꜱᴛ ꜱʏꜱᴛᴇᴍ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ 94775587705*`,
+        errorMessage: "*千ᴀɪʟᴇᴅ ᴛᴏ ᴊᴏɪɴ ɢʀᴏᴜᴘꜱ ᴏʀ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ. Ｅʀʀᴏʀ: {ᴇʀʀᴏʀ}* ✖️",
+        broadcastCaption: `*卂ꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ ʙʀᴏᴀᴅᴄᴀꜱᴛ ꜱʏꜱᴛᴇᴍ එක මගින් ලැබෙන 爪ᴇꜱꜱᴀɢᴇ එකකි ‼️*\n\n*Group Name :* {groupName}\n\n{caption}\n\n*乃ʀᴏᴀᴅᴄᴀꜱᴛ ꜱʏꜱᴛᴇᴍ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ 94775587705*`,
         adminCaption: `*ㄒ几乂 ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ ʏᴏᴜʀ ꜱʏꜱᴛᴇᴍ ɪꜱ ʙᴇꜱᴛ ‼️*\n\nGroup: {groupName}\n\n{caption}\n\n*Ｐᴏᴡᴇʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ*`,
         vcfCaption: `*Ｃᴏɴᴛᴀᴄᴛ ꜱʜᴀʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ 🛒*`
     },
     
     media: {
-        broadcastImage: "https://ibb.co/nNVjBQZK"
+        broadcastImage: "https://i.ibb.co/DPNY0q6s/20250610-173824.jpg"
     }
 };
